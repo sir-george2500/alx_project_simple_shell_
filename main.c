34656d49eb07fdbf62dfd5366aa2d00887089e97
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-08-03 12:20:09 +0000

    added existing feature to the gshell

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "main.h"
-
+/**
+ * main - Entry point for the Gshell program.
+ *
+ * @argc: The number of command-line arguments (unused).
+ * @argv: An array of pointers to command-line argument strings (unused).
+ *
+ * Return: 0 on successful execution, -1 on error.
+ */
 int main(int argc, char **argv)
 {
 
 char *prompt = "(Gshell) $";
 char *lineptr;
 size_t n = 0;
+ssize_t nchars_read;
 
 /* declaring void variables */
 (void)argc;
@@ -16,7 +24,13 @@ while (1)
 {
 
 	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
+	nchars_read = getline(&lineptr, &n, stdin);
+
+	if (nchars_read == -1)
+	{
+		printf(" Exiting the shell ....\n");
+		return (-1);
+	}
 
 	printf("%s\n", lineptr);
 	 /* free up allocated memory */
